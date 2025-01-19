38e6a5c39c8d0ebaa493e3f76aa4526100ccbd71
ivanhomziak <ivan.homziak@gmail.com>
2025-01-19 22:16:26 +0100

    ref

---
+++ src/main/java/com/ihomziak/api/jwtauthorities/JwtAuthoritiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihomziak.api.jwtauthorities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class JwtAuthoritiesApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthoritiesApplicationTests.class, args);
+    }
+
+}
